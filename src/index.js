35557ade595acf2fc3e01e799256b007d7de9e50
yourd8ta-code <yourd8ta@gmail.com>
2025-12-05 15:12:58 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.js"; // Make sure the .js extension is here
+import App from "./App.js"; // <-- include .js here
+
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
